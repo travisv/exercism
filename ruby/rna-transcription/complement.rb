7e968d0fdd0ed0ed9631149fdd7ce422fb88a584
travisv <vandusseldorptravis@gmail.com>
2015-03-26 01:04:55 -0500

    Second attempt

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,41 +1,22 @@
 class Complement
+  DNA_COMPLEMENTS = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }
+  RNA_COMPLEMENTS = { 'C' => 'G', 'G' => 'C', 'U' => 'A', 'A' => 'T' }
 
-def self.of_dna(rna)
-  dna = []
-  length = [rna.length].min
-  (0...length).each do |i|
-    if rna[i] == 'G'
-      dna << 'C'
-    elsif rna[i] == 'C'
-      dna << 'G'
-    elsif rna[i] == 'T'
-      dna << 'A'
-    elsif rna[i] == 'A'
-      dna << 'U'
-    elsif rna[i] == 'U'
-      raise ArgumentError.new()
+  def self.of_dna(rna)
+    sequence = rna.split(//)
+    sequence.collect! do |t|
+      fail ArgumentError unless DNA_COMPLEMENTS.include?(t)
+      DNA_COMPLEMENTS[t]
     end
+    sequence.join
   end
-  dna.join
-end
 
-def self.of_rna(dna)
-  rna = []
-  length = [dna.length].min
-  (0...length).each do |i|
-    if dna[i] == 'G'
-      rna << 'C'
-    elsif dna[i] == 'C'
-      rna << 'G'
-    elsif dna[i] == 'U'
-      rna << 'A'
-    elsif dna[i] == 'A'
-      rna << 'T'
-    elsif dna[i] == 'T'
-      raise ArgumentError.new()
+  def self.of_rna(dna)
+    sequence = dna.split(//)
+    sequence.collect! do |t|
+      fail ArgumentError unless RNA_COMPLEMENTS.include?(t)
+      RNA_COMPLEMENTS[t]
     end
+    sequence.join
   end
-  rna.join
 end
-    end
-
