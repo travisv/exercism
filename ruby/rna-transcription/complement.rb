1263eedb4c33306331242d8794b656e3a780300f
travisv <vandusseldorptravis@gmail.com>
2015-03-27 00:13:05 -0500

    Third attempt

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,22 +1,14 @@
 class Complement
-  DNA_COMPLEMENTS = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }
-  RNA_COMPLEMENTS = { 'C' => 'G', 'G' => 'C', 'U' => 'A', 'A' => 'T' }
-
   def self.of_dna(rna)
-    sequence = rna.split(//)
-    sequence.collect! do |t|
-      fail ArgumentError unless DNA_COMPLEMENTS.include?(t)
-      DNA_COMPLEMENTS[t]
-    end
-    sequence.join
+    transcribe(rna, 'CGTA', 'GCAU', 'U')
   end
 
   def self.of_rna(dna)
-    sequence = dna.split(//)
-    sequence.collect! do |t|
-      fail ArgumentError unless RNA_COMPLEMENTS.include?(t)
-      RNA_COMPLEMENTS[t]
-    end
-    sequence.join
+    transcribe(dna, 'CGUA', 'GCAT', 'T')
+  end
+
+  def self.transcribe(genome_type, bases, complement, incompatible)
+    fail ArgumentError if genome_type.include?(incompatible)
+    genome_type.tr(bases, complement)
   end
 end
